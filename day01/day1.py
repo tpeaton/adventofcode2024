dab87f3f0767b555afcf023c8ba948225973a258
Tom Eaton <tomeaton@cisco.com>
2024-12-02 16:15:21 -0600

    day 1 part 1

--- day01/day1.py
+++ day01/day1.py
@@ -1,8 +1,31 @@
 # Advent of Code 2024 Day 1
 
+def sum_difference(list1, list2):
+    total_difference = 0
+    for i, j in zip(list1, list2):
+        total_difference += abs(i - j)
+
+    return total_difference
+
+
+def read_file(filename, verbose=False):
+    list1 = []
+    list2 = []
+    with open(filename, 'r') as file:
+        for record in file:
+            record = [int(x) for x in record.split()]
+            list1.append(record[0])
+            list2.append(record[1])
+
+    return sorted(list1), sorted(list2)
+
 
 def main():
-    pass
+    list1, list2 = read_file('input.txt')
+
+    total_difference = sum_difference(list1, list2)
+
+    print('total difference: ', total_difference)
 
 if __name__ == "__main__":
     main()
