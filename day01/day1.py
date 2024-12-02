685af2a25e5c9876531134015046ed5255ce184b
Tom Eaton <tomeaton@cisco.com>
2024-12-02 15:24:07 -0600

    refactoring

---
+++ day01/day1.py
@@ -0,0 +1,8 @@
+# Advent of Code 2024 Day 1
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- day02/day2.py
+++ day02/day2.py
@@ -1,57 +1,70 @@
 # Advent of Code 2024 Day 2
 
 
+def is_difference_too_great(num, max_difference=3, verbose=False):
+    if abs(num) > max_difference:
+        if verbose:
+            print('ERROR: difference too great')
+        return True
+    return False
+
+
+def are_numbers_equal(num, verbose=False):
+    if num == 0:
+        if verbose:
+            print('ERROR: adjacent numbers are equal')
+        return True
+    return False
+
+
+def did_direction_change(num1, num2):
+    """Will return True if the numbers are on opposite sides of zero."""
+    return (num1 * num2) < 0
+
 def is_safe(record, verbose=False):
-    is_descending = False
-    is_ascending = False
-    position = 0
+    previous_direction = record[0] - record[1]
 
     for pair in zip(record, record[1:]):
-
         difference = pair[0] - pair[1]
+
         if verbose:
             print(pair, 'diff:', difference)
 
-        if abs(difference) > 3:
-            if verbose:
-                print('FAIL: difference > 3')
+        if is_difference_too_great(difference, verbose=verbose):
             return False
-        if difference == 0:
-            if verbose:
-                print('FAIL: difference == 0')
-            return False
-        if difference > 0:
-            if verbose:
-                print('Descending numbers')
-            is_descending = True
-        else:
-            if verbose:
-                print('Ascending numbers')
-            is_ascending = True
 
-        if is_descending and is_ascending:
-            if verbose:
-                print('FAIL: direction switched')
+        if are_numbers_equal(difference, verbose=verbose):
             return False
+
+        if did_direction_change(previous_direction, difference):
+            if verbose:
+                print('ERROR: direction changed')
+            return False
+
+        previous_direction = difference
 
     return True
 
 
 def main():
+
     total_count = 0
     safe_count = 0
 
-    with open('input.txt', 'r') as file:
+    with open('input2.txt', 'r') as file:
         for record in file:
             record = [int(x) for x in record.split()]
             total_count += 1
 
+            print('***************************************************')
+            print()
             print('analyzing record:', record)
-            if is_safe(record, verbose=False):
+            if is_safe(record, verbose=True):
                 safe_count += 1
                 print('SAFE')
             else:
                 print('NOT SAFE')
+            print()
 
     print('total records: ', total_count)
     print('safe records: ', safe_count)
