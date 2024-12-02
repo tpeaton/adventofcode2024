21d746b6200e678fb24dc12e3b3f217010e33245
Tom Eaton <tomeaton@cisco.com>
2024-12-02 16:25:07 -0600

    Day 1 Part 2

--- day01/day1.py
+++ day01/day1.py
@@ -1,5 +1,14 @@
 # Advent of Code 2024 Day 1
 
+
+def calculate_similarity_score(list1, list2):
+    score = 0
+    for num in list1:
+        score += num * list2.count(num)
+
+    return score
+
+
 def sum_difference(list1, list2):
     total_difference = 0
     for i, j in zip(list1, list2):
@@ -23,9 +32,14 @@ def read_file(filename, verbose=False):
 def main():
     list1, list2 = read_file('input.txt')
 
+    # Part One
     total_difference = sum_difference(list1, list2)
-
     print('total difference: ', total_difference)
+
+    # Part Two
+    similarity_score = calculate_similarity_score(list1, list2)
+    print('similarity score: ', similarity_score)
+
 
 if __name__ == "__main__":
     main()
